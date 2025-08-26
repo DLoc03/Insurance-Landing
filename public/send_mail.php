6746c19097418563c6a3e874d31d507023f44c62
Dloc03 <locn9730@gmail.com>
2025-08-26 13:23:39 +0700

    Change destination email address

--- public/send_mail.php
+++ public/send_mail.php
@@ -1,7 +1,7 @@
 <?php
 function sendContactMailWithPHPMail(array $data):bool|string {
 
-    $to="locn9730@gmail.com";
+    $to="tuyenpham103@gmail.com";
     $subject =$data['subject']??"Thông tin đăng ký dịch vụ bảo hiểm";
 
     $message = <<<EOD
@@ -13,12 +13,12 @@ Lời nhắn kèm theo: {$data['text']}
 EOD;
 
 
-    $headers = "From: SuriShop Contact <no-reply@surishops.com>\r\n";
+    $headers = "From: Baohiemyeuthuong Contact <no-reply@baohiemyeuthuong.com>\r\n";
     $headers .= "Reply-To: {$data['email']}\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    $parameters = "-f no-reply@surishops.com";
+    $parameters = "-f no-reply@baohiemyeuthuong.com";
     
     if (mail($to, $subject, $message, $headers, $parameters)) {
         return true;
